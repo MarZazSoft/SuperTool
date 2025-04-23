665a623e043e98a63711b04fce6fccfd70c6169a
DavidKPV <david.mm.kpv@gmail.com>
2025-04-22 19:47:46 -0600

    DEV-Add-google-button

--- SuperTool/app/src/main/java/com/marzazsoft/supertool/ui/screens/MainScreen.kt
+++ SuperTool/app/src/main/java/com/marzazsoft/supertool/ui/screens/MainScreen.kt
@@ -1,15 +1,33 @@
 package com.marzazsoft.supertool.ui.screens
 
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.marzazsoft.supertool.R
+import com.marzazsoft.supertool.ui.theme.darkBlue
+import com.marzazsoft.supertool.ui.theme.lightBlue
+import com.marzazsoft.supertool.utils.SIMPLE_PADDING
 
 @Suppress("ktlint:standard:function-naming")
 @Composable
@@ -17,19 +35,68 @@ fun MainScreen(
     navController: NavHostController,
     modifier: Modifier,
 ) {
-    ConstraintLayout(modifier = modifier.fillMaxWidth().fillMaxHeight()) {
-        val textExample = createRef()
+    Column(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(Brush.verticalGradient(listOf(lightBlue, darkBlue), startY = 0f, endY = 1500f))
+                .padding(all = SIMPLE_PADDING),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().height(100.dp).background(Color.Red),
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_google),
+                contentDescription = "",
+                modifier = Modifier.weight(2f),
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.height(100.dp).weight(4f),
+            ) {
+                Text(text = "Título")
+                Text(text = "Contenido")
+            }
+        }
 
-        Text(
-            "INICIO SUPER TOOL - WORKING ON IT... :D",
-            modifier =
-                Modifier.constrainAs(textExample) {
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth().height(100.dp).background(Color.Yellow),
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_google),
+                contentDescription = "",
+                modifier = Modifier.weight(2f),
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.height(100.dp).weight(4f),
+            ) {
+                Text(text = "Título")
+                Text(text = "Contenido")
+            }
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth().height(100.dp).background(Color.Green),
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_google),
+                contentDescription = "",
+                modifier = Modifier.weight(2f),
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.height(100.dp).weight(4f),
+            ) {
+                Text(text = "Título", fontWeight = FontWeight.ExtraBold)
+                Text(text = "Contenido")
+            }
+        }
     }
 }
 
