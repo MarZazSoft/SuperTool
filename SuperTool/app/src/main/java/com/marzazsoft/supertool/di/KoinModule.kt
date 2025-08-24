761c05e3abea71e2a257dad3b070076f00af23a5
DavidKPV <david.mm.kpv@gmail.com>
2025-08-23 20:51:27 -0600

    [UPDATE] Ktlint corrections

--- SuperTool/app/src/main/java/com/marzazsoft/supertool/di/KoinModule.kt
+++ SuperTool/app/src/main/java/com/marzazsoft/supertool/di/KoinModule.kt
@@ -1,12 +1,12 @@
 package com.marzazsoft.supertool.di
 
 import androidx.credentials.CredentialManager
-import com.marzazsoft.supertool.repositories.DataStoreRepository
 import com.marzazsoft.supertool.data.dataStore
 import com.marzazsoft.supertool.presentation.viewModels.MainViewModel
 import com.marzazsoft.supertool.presentation.viewModels.ProfileViewModel
 import com.marzazsoft.supertool.presentation.viewModels.SplashViewModel
 import com.marzazsoft.supertool.presentation.viewModels.ToolsViewModel
+import com.marzazsoft.supertool.repositories.DataStoreRepository
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
--- SuperTool/app/src/main/java/com/marzazsoft/supertool/presentation/viewModels/MainViewModel.kt
+++ SuperTool/app/src/main/java/com/marzazsoft/supertool/presentation/viewModels/MainViewModel.kt
@@ -17,9 +17,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.marzazsoft.mobile.supertool.common.utils.ApiStatus
 import com.marzazsoft.supertool.BuildConfig
-import com.marzazsoft.supertool.repositories.DataStoreRepository
 import com.marzazsoft.supertool.models.SignInMethod
 import com.marzazsoft.supertool.models.User
+import com.marzazsoft.supertool.repositories.DataStoreRepository
 import com.marzazsoft.supertooldesign.utils.TAG_LOG
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
